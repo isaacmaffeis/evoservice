0c6ff9f82a75047fe4944f03a478ece377cf6dfa
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-09 18:08:07 +0200

    add clean and export scripts

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,6 +7,15 @@ echo "Copying Generated Test Cases into the project..."
 cp -r evosuite-tests/* src/test/java/
 
 echo "Running the mvn test phase..."
-mvn test
+if mvn test; then
+    echo "Tests passed. Exporting the tests..."
+    mkdir -p ./output/
+    cp -r evosuite-tests/* src/test/java/
+else
+    echo "Tests failed. Export aborted."
+fi
+
+echo "Cleaning the project"
+python3 scripts/clean.py
 
 echo "tests.sh process completed."
